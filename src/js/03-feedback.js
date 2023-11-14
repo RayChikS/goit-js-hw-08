d9420f67d66b2447a1dcc7627856ebc064471a76
RayChikS <vatoresp@gmail.com>
2023-11-14 13:24:37 +0300

    third-1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
+form.addEventListener('input', function (event) {
+  const data = {
+    email: event.target.value,
+    message: event.target.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(data));
+});
