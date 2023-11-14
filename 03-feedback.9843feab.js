796ce34b1cddcb2bbd281f3b1d88f63be1bd1315
RayChikS <127132649+RayChikS@users.noreply.github.com>
2023-11-14 11:24:57 +0000

    Deploying to gh-pages from @ RayChikS/goit-js-hw-08@d9420f67d66b2447a1dcc7627856ebc064471a76 🚀

---
+++ 03-feedback.9843feab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){var t={email:e.target.value,message:e.target.value};localStorage.setItem("feedback-form-state",JSON.stringify(t))}))}();
+//# sourceMappingURL=03-feedback.9843feab.js.map
---
+++ 03-feedback.b62ca017.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){const t={email:e.target.value,message:e.target.value};localStorage.setItem("feedback-form-state",JSON.stringify(t))}));
+//# sourceMappingURL=03-feedback.b62ca017.js.map
