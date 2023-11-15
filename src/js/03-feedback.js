bc21c8519144c505e14958deafda4bb694f950e5
RayChikS <vatoresp@gmail.com>
2023-11-15 14:43:49 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,41 @@
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
 const localStorageKey = 'feedback-form-state';
 
-form.addEventListener('input', function (event) {
-  const data = {
-    email: event.target.value,
-    message: event.target.value,
-  };
-  localStorage.setItem(localStorageKey, JSON.stringify(data));
+let data = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener(
+  'input',
+  throttle(function (event) {
+    data = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(data));
+  }),
+  500
+);
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log(data);
+  form.reset();
 });
+
+function toShow() {
+  const saved = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (saved.email) {
+    emailInput.value = saved.email;
+  }
+
+  if (saved.message) {
+    messageInput.value = saved.message;
+  }
+}
+toShow();
